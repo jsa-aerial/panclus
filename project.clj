cb79518901857a813142c67c58e3fd178ff9c647
jsa-aerial <jsa.aerial@gmail.com>
2018-06-20 10:47:08 -0400

    move to nrepl/cider 17

--- project.clj
+++ project.clj
@@ -4,15 +4,17 @@
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [lambda-ml           "0.1.0"]
+                 [org.clojure/tools.reader "1.0.0-beta3"]
+                 [org.clojure/tools.nrepl  "0.2.13"] ; Explicit nREPL
+                 [org.clojure/tools.cli    "0.3.3"] ; cmd line arg processing
+
+                 [lambda-ml "0.1.0"]
                  [hiccup "1.0.5"]
                  [expresso "0.2.2"]
                  [aysylu/loom "0.5.4"]
-                 [loom-gorilla "0.1.0"]
-                 [gorilla-renderable "2.0.0"]
-                 [gorilla-plot "0.1.4"]
+                 [kixi/stats "0.4.0"]
 
-                 [gyptis "0.2.2"]
+                 [vgl "0.1.0"]
 
                  [org.clojure/data.csv "0.1.2"]
                  [net.apribase/clj-dns "0.1.0"]
@@ -20,15 +22,14 @@
                  [aerial.utils "1.2.0"]
                  [aerial.bio.utils "2.0.0"]]
 
-  :main ^:skip-aot gorilla-test.core
   :target-path "target/%s"
 
-  :plugins [[lein-gorilla "0.4.0" :exclusions [org.clojure/clojure]]
-            ^:replace
-            [refactor-nrepl    "2.2.0"]
-            [cider/cider-nrepl "0.12.0"]]
+  :plugins [^:replace
+            [cider/cider-nrepl "0.17.0"]
+            [refactor-nrepl    "2.4.0-SNAPSHOT"]]
 
   :profiles {:uberjar {:aot :all}}
+  :main panclus.core
 
   :repositories [["lclrepo" "file:lclrepo"]]
   :source-paths ["src"]
